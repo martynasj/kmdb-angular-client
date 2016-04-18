bdb1e4a8772b0444cfb6ed449972c05403dc7b3e
martynas <mj@svift.net>
2016-04-18 21:55:46 +0200

    handle no response from server

--- app/pages/tv/tvList.js
+++ app/pages/tv/tvList.js
@@ -3,31 +3,37 @@ import tvCard from '../../components/tvCard';
 
 const app = angular.module('app');
 
+
 function controller(apiService) {
   // save 'this' reference
   const ctrl = this;
 
   ctrl.dataLoading = true;
+  ctrl.error = false;
 
   this.$onInit = function() {
     apiService.getAllSeries().then(function success(result) {
       ctrl.allSeries = result;
       ctrl.dataLoading = false;
     }, function error(reason) {
-      console.log(reason);
+      ctrl.error = true;
+      ctrl.dataLoading = false;
     });
   };
 
 }
 
 const template = `
-              <p ng-show="$ctrl.dataLoading">Loading...</p>
+              <div ng-if="$ctrl.dataLoading">Loading...</div>
+              <div ng-if="$ctrl.error">No connection to the server</div>
               <div class="">
                 <div ng-repeat="series in $ctrl.allSeries" class="col s6 m4 l3">
                   <tv-card series="series"></tv-card>
                 </div>
               </div>
 `;
+
+
 
 export default app.component('tvList', {
   template,
